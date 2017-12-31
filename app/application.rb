283a22f872ccbadaf6b493b242654320fb8287bf
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 21:58:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -29,7 +29,7 @@ class Application
         @@cart < search_term
         resp.write("added #{req}")
       else
-        resp.write "#{env}\n"
+        resp.write "#{search_term}\n"
         resp.write " don't have that item\n"
       end
     else
